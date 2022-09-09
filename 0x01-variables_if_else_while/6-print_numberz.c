357829cdd5a7719c7890adb53abd2a178080fe40
Ronbi <akinronbi123@gmail.com>
2022-09-09 11:03:14 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main  - Prints numbers betwween 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	for (digit = 48; digit < 58; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
+
