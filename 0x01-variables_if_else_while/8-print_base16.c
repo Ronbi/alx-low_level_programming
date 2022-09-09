7df9e26e03595621c1a878e9cc390d75bed94ad7
Ronbi <akinronbi123@gmail.com>
2022-09-09 11:15:23 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -prints number of base 16
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+	for (digit = '0'; digit <= '9'; digit++)
+                 putchar(digit);
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+		putchar(digit);
+	putchar('\n');
+	return (0);
+}
