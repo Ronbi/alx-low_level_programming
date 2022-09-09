b6fb7f94574f068e59534bba11fbb53fd254f51b
Ronbi <akinronbi123@gmail.com>
2022-09-09 10:02:09 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - assigns random numbebr to the variable n
+ *
+ * Return: Always 0 (Success)
+ */  
+  int main(void)
+{
+	int n;
+	n=0-10;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n" , n);
+	else
+		printf("%d is negative\n", n);
+	return (0)
+}
