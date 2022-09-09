ba5a7d231acd77177dd0c586f9d634d05def36f6
Ronbi <akinronbi123@gmail.com>
2022-09-09 13:58:41 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,175 +8,20 @@
  */
 int main (void)
 {
-	int i, l;
+	int n, l;
+
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-       
+
+
         if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	        printf("Last digit of %d is %d and is greater than 5\n", n, l);
 
 	else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
+	        printf("Last digit of %d is %d and is 0\n", n, l);
 	else
 
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
+          printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);        
 	return (0);
-
-} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							} 10;
-       
-        if (l > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, l);
-
-		else if (l == 0)
-
-			printf("Last digit of %d is %d and is 0\n", n, l);
-
-				else
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-
-
-
-							return (0);
-
-							}
+}
