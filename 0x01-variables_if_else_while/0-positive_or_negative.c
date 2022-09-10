08794db0040e66cf5da0d9600ba80d1ec05af64e
Ronbi <akinronbi123@gmail.com>
2022-09-10 09:18:14 -0700

    Task 8

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+/**
+ * Main - assigns random numbebr to the variable n
+ *
+ * Return: Always 0 (Success)
+ */  
+  int main(void)
+{
+	int n;
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main -prints number of base 16
  *
@@ -6,11 +7,14 @@
  */
 int main(void)
 {
+	int digit;
+
 	for (digit = '0'; digit <= '9'; digit++)
                  putchar(digit);
 
 	for (digit = 'a'; digit <= 'f'; digit++)
 		putchar(digit);
+
 	putchar('\n');
 	return (0);
 }
