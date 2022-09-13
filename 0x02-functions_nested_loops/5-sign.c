d3b6d294e01c98fb1e73ddfcc4fe0a318a92e554
Ronbi <akinronbi123@gmail.com>
2022-09-13 07:42:59 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *  print_sign - prints the sign of a numver
+ *
+ *  Return: 1 and prints + if n is greater than zero
+ */
+int print_sign(int n)
+{
+	
+	if (n > 0)
+	{
+
+	putchar('+');
+	return (1);
+
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+	        return (0);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+
+	}
+
+	return (-1);
+
+}
+
