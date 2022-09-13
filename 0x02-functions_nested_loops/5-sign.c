bbad343697e8bb5be9ea6b2f1d85ffa6b184379e
Ronbi <akinronbi123@gmail.com>
2022-09-13 07:50:03 -0700

    Task 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,13 @@ int print_sign(int n)
 	
 	if (n > 0)
 	{
-
-	putchar('+');
-	return (1);
+          _putchar('+');
+	  return (1);
 
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		_putchar(48);
 	        return (0);
 	}
 
