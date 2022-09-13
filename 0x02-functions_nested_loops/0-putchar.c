45e5ab15f2c575cf0bd91b2f4261112b8d36ffa3
Ronbi <akinronbi123@gmail.com>
2022-09-13 05:49:33 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - program that prints "_putchar", followed by a mew line
+ *
+ * Return: 0 Always success
+ */
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
